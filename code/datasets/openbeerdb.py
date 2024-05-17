fc9751818ad79e44c615d884d22f4bbc77dea464
Daan Brugmans <daanbrugmans@gmail.com>
2024-05-17 17:45:59 +0200

    Finished Assignment 3

--- code/datasets/openbeerdb.py
+++ code/datasets/openbeerdb.py
@@ -4,6 +4,17 @@ import pandas as pd
 
 class OpenBeerDBDataset:
     def __init__(self) -> None:
+        query = """
+        SELECT beers.name AS beer_name, breweries.name AS brewery_name, categories.cat_name AS category, styles.style_name AS style, beers.abv, beers.ibu, beers.descript
+        FROM beers
+        INNER JOIN breweries
+            ON beers.brewery_id = breweries.id
+        INNER JOIN categories
+            ON beers.cat_id = categories.id
+        INNER JOIN styles
+            ON beers.style_id = styles.id
+        """
+        
         if str(Path.cwd()).endswith("code"):
             cwd = Path.cwd().parents[0]
         else:
